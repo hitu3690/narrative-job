591dce0625232a6cb1319d9500007f3a7bbdd949
Yosuke Iida <hitu3690@icloud.com>
2022-02-22 20:08:41 +0900

    ビジネスロジックを切り出し

--- src/BookSearchDialog.tsx
+++ src/BookSearchDialog.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { useBookData } from 'useBookData'
 import { BookDescription } from './BookDescription'
 import BookSearchItem from './BookSearchItem'
 
@@ -9,56 +10,20 @@ type BookSearchDialogProps = {
 /* BookDialogコンポーネントの定義 */
 const BookSearchDialog = (props: BookSearchDialogProps) => {
   /**
-   * setXXの関数呼び出しを通じてステート変数が変化すると、それを検知してコンポーネントの再レンダリングを行う。（つまり、コンポーネント関数が呼び出される）
-   * handleTitleInputChangeの引数に'A'を入力すると、最初のuseState('')->は、useState('A')
-   * ちなみに親コンポーネントから渡されるpropsの値が変更された場合も、子コンポーネントは再レンダリングされる
+   * refは、「DOM要素への参照を保持する」、「あらゆる書き換え可能な値を保持しておく」のに使える
+   * また、その中身が変わってもそのことを通知しないので、コンポーネントの再レンダリングは発生しない
    */
-  const [books, setBooks] = useState([] as BookDescription[])
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  /**
-   * 検索ロジック
-   *  - isSearching -> 検索処理実行中であることを示すステート管理
-   * useEffectを使ってステート変数の副作用を実装
-   *  - useEffectの第二引数には、「副作用が依存するステート変数 or propsのプロパティ」の配列を渡す
-   *  - この配列の中身が変更を検知した場合のみ、副作用を呼び出す。
-   *  - つまり、isSearching, title, author, props.maxResultsの中で検知できるが、今回はisSearchingのみ検知させる
-   */
-  const [isSearching, setIsSearching] = useState(false)
-  useEffect(() => {
-    if (isSearching) {
-      const url = buildSearchUrl(title, author, props.maxResults)
-      fetch(url)
-        /* リクエストURLを生成してAPIコールし、レスポンスを返す */
-        .then((res) => {
-          console.log(url)
-          return res.json()
-        })
-        /* レスポンス結果を整形して、booksを抽出 */
-        .then((json) => {
-          console.log(json)
-          return extractBooks(json)
-        })
-        /* ステートの更新 */
-        .then((books) => {
-          console.log(books)
-          setBooks(books)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    }
-    setIsSearching(false)
-  }, [isSearching])
-  const handleTitleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value)
-  }
-  const handleAuthorInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setAuthor(e.target.value)
-  }
+  const titleRef = useRef<HTMLInputElement>(null)
+  const authorRef = useRef<HTMLInputElement>(null)
+  /* 独自ステート変数を定義して、ステートbookを別ファイルに切り出す */
+  const [books, setIsSearching] = useBookData(
+    titleRef.current ? titleRef.current!.value : '',
+    authorRef.current ? authorRef.current!.value : '',
+    props.maxResults,
+  )
   const handleSearchClick = () => {
-    if (!title && !author) {
-      alert('入力してください')
+    if (!titleRef.current!.value && !authorRef.current!.value) {
+      alert('条件を入力してください')
       return
     }
     /**
@@ -83,16 +48,8 @@ const BookSearchDialog = (props: BookSearchDialogProps) => {
     <div className="dialog">
       <div className="operation">
         <div className="conditions">
-          <input
-            type="text"
-            onChange={handleTitleInputChange}
-            placeholder="タイトルで検索"
-          />
-          <input
-            type="text"
-            onChange={handleAuthorInputChange}
-            placeholder="著者名で検索"
-          />
+          <input type="text" ref={titleRef} placeholder="タイトルで検索" />
+          <input type="text" ref={authorRef} placeholder="著者名で検索" />
         </div>
         <div className="button-like" onClick={handleSearchClick}>
           検索
@@ -101,36 +58,6 @@ const BookSearchDialog = (props: BookSearchDialogProps) => {
       <div className="search-results">{bookItems}</div>
     </div>
   )
-}
-/* リクエストするURLを整形 */
-function buildSearchUrl(
-  title: string,
-  author: string,
-  maxResults: number,
-): string {
-  let url = 'https://www.googleapis.com/books/v1/volumes?q='
-  const conditions: string[] = []
-  if (title) {
-    conditions.push(`intitle: ${title}`)
-  }
-  if (author) {
-    conditions.push(`inauthor: ${author}`)
-  }
-  return url + conditions.join('+') + `&maxResults=${maxResults}`
-}
-/* レスポンスされたJSONを整形 */
-function extractBooks(json: any): BookDescription[] {
-  const items: any[] = json.items
-  return items.map((item: any) => {
-    const { volumeInfo } = item
-    return {
-      title: volumeInfo.title,
-      authors: volumeInfo.authors ? volumeInfo.authors.join(', ') : '',
-      thumbnail: volumeInfo.imageLinks
-        ? volumeInfo.imageLinks.smallThumbnail
-        : '',
-    }
-  })
 }
 
 export default BookSearchDialog
---
+++ src/useBookData.ts
@@ -0,0 +1,82 @@
+import { useState, useEffect, useRef } from 'react'
+import { BookDescription } from 'BookDescription'
+
+/* リクエストするURLを整形 */
+function buildSearchUrl(
+  title: string,
+  author: string,
+  maxResults: number,
+): string {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q='
+  const conditions: string[] = []
+  if (title) {
+    conditions.push(`intitle: ${title}`)
+  }
+  if (author) {
+    conditions.push(`inauthor: ${author}`)
+  }
+  return url + conditions.join('+') + `&maxResults=${maxResults}`
+}
+/* レスポンスされたJSONを整形 */
+function extractBooks(json: any): BookDescription[] {
+  const items: any[] = json.items
+  return items.map((item: any) => {
+    const { volumeInfo } = item
+    return {
+      title: volumeInfo.title,
+      authors: volumeInfo.authors ? volumeInfo.authors.join(', ') : '',
+      thumbnail: volumeInfo.imageLinks
+        ? volumeInfo.imageLinks.smallThumbnail
+        : '',
+    }
+  })
+}
+
+export const useBookData = (
+  title: string,
+  author: string,
+  maxResults: number,
+) => {
+  /**
+   * setXXの関数呼び出しを通じてステート変数が変化すると、それを検知してコンポーネントの再レンダリングを行う。（つまり、コンポーネント関数が呼び出される）
+   * handleTitleInputChangeの引数に'A'を入力すると、最初のuseState('')->は、useState('A')
+   * ちなみに親コンポーネントから渡されるpropsの値が変更された場合も、子コンポーネントは再レンダリングされる
+   */
+  const [books, setBooks] = useState([] as BookDescription[])
+  /**
+   * 検索ロジック
+   *  - isSearching -> 検索処理実行中であることを示すステート管理
+   * useEffectを使ってステート変数の副作用を実装
+   *  - useEffectの第二引数には、「副作用が依存するステート変数 or propsのプロパティ」の配列を渡す
+   *  - この配列の中身が変更を検知した場合のみ、副作用を呼び出す。
+   *  - つまり、isSearching, title, author, props.maxResultsの中で検知できるが、今回はisSearchingのみ検知させる
+   */
+  const [isSearching, setIsSearching] = useState(false)
+  useEffect(() => {
+    if (isSearching) {
+      const url = buildSearchUrl(title, author, maxResults)
+      fetch(url)
+        /* リクエストURLを生成してAPIコールし、レスポンスを返す */
+        .then((res) => {
+          console.log(url)
+          return res.json()
+        })
+        /* レスポンス結果を整形して、booksを抽出 */
+        .then((json) => {
+          console.log(json)
+          return extractBooks(json)
+        })
+        /* ステートの更新 */
+        .then((books) => {
+          console.log(books)
+          setBooks(books)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+    setIsSearching(false)
+  }, [isSearching])
+  /* 複数の型の値を配列で返却して呼び出し元でデストラクチャリング（分割代入）で受け取るには、上記のように as const を付ける */
+  return [books, setIsSearching] as const
+}
